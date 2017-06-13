2c5d3749a308241e41e9db89efa08a0b707d560f
xieyi <xieyie@qq.com>
2017-06-13 17:21:54 +0800

    合并

--- src/main/java/uk/co/caprica/vlcjplayer/MainPlayer.java
+++ src/main/java/uk/co/caprica/vlcjplayer/MainPlayer.java
@@ -213,9 +213,8 @@ public class MainPlayer {
         System.out.println("这里是被 Lambda 转换过的分支里面的数据");
         System.out.println("这里是被 Lambda 转换过的分支里面的数据");
         System.out.println("这里是被 Lambda 转换过的分支里面的数据");
-        System.out.println("这里是被 Lambda 转换过的分支里面的数据");
 
-        
+
 
     }
 
