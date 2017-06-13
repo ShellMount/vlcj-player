b4acd5c6bfc7b273752dd11f3bb2fa591e9c01d7
xieyi <xieyie@qq.com>
2017-06-13 17:47:54 +0800

    添加TESTGIT

---
+++ src/main/java/uk/co/caprica/vlcjplayer/view/TestGit.java
@@ -0,0 +1,10 @@
+package uk.co.caprica.vlcjplayer.view;
+
+/**
+ * Created by ShellMount on 2017/6/13.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("在GIT的模式下，是否能够上传？");
+    }
+}
